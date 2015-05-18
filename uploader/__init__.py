45f277a33b25783eb63352f9048a3447a9a158ef
Corey Oordt <coreyoordt@gmail.com>
2015-05-18 08:06:09 -0500

    Version 0.2

--- uploader/__init__.py
+++ uploader/__init__.py
@@ -5,8 +5,8 @@ any file type in the Django Admin.
 """
 __version_info__ = {
     'major': 0,
-    'minor': 1,
-    'micro': 1,
+    'minor': 2,
+    'micro': 0,
     'releaselevel': 'final',
     'serial': 1
 }
